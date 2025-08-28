0d822d165f2e42577e5f6985f02e404fe7e92e2f
NATASHA PRADHAN <natasha@acehours.com>
2025-08-28 21:14:28 +0530

    +3: All tests passed -> feat: implement single number parsing

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,8 @@
 class StringCalculator {
   static int add(String numbers) {
     if (numbers.isEmpty) return 0;
+    final n = int.tryParse(numbers);
+    if (n != null) return n;
     throw UnimplementedError();
   }
 }
