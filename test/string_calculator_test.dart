81d57d6fc2b2e98ae2e34d24b1bb839716bb5769
NATASHA PRADHAN <natasha@acehours.com>
2025-08-28 21:13:06 +0530

    +2: -1: Some tests failed -> test: single number returns its value

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -5,4 +5,9 @@ void main() {
   test('empty string returns 0', () {
     expect(StringCalculator.add(''), equals(0));
   });
+
+  test('single number returns its value', () {
+    expect(StringCalculator.add('7'), equals(7));
+    expect(StringCalculator.add('42'), equals(42));
+  });
 }
